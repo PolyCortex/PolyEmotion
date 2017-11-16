3a030d51443be20542c28d02448ca4bde17f3547
Christophe Kedzierski-Tanguay <CKTanguay@gmail.com>
2017-11-16 18:10:07 -0500

    Push pour voir la branche

--- MusicPlayerGUI/MusicPlayerGUI/Form1.Designer.cs
+++ MusicPlayerGUI/MusicPlayerGUI/Form1.Designer.cs
@@ -33,19 +33,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 171);
+            this.button1.Location = new System.Drawing.Point(183, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(229, 89);
+            this.button1.Size = new System.Drawing.Size(204, 71);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 355);
+            this.ClientSize = new System.Drawing.Size(949, 471);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
