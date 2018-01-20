230f4bdf102d64178830f47bac57fcd04cb2b78f
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2018-01-20 02:33:18 -0500

    Improvement to the FFT plot (I still need to look at the xAxis)-AM)

--- OpenBCI_Python/basic_example_outlet.py
+++ OpenBCI_Python/basic_example_outlet.py
@@ -11,24 +11,25 @@ i = 0.0
 rep_time = 250
 print("now sending data...")
 while True:
-    if i == rep_time: 
+    if i == rep_time:
         i = 0.0
 
     time.sleep(0.004)
-    
-    sinus = sin(i/rep_time * 2 * pi)
-    if 239 < i < 249: 
-        mysample = [2, 2, 2, 2,
-                    2, 2, 2, 2]
-    else: 
-        mysample = [sinus, rand(), sinus, rand(),
-            sinus, sinus, rand(), sinus]
-    outlet.push_sample(mysample)
-    
-    i += 1
-    
 
-#%% TEST fft ploting  TODO: (ALEX) 
+    sinus1 = sin(i/rep_time * 10 * pi)
+    sinus2 = sin(i/rep_time * 20 * pi)
+    if 239 < i < 249:
+        mysample = [sinus2, sinus1+sinus2, 3, 4,
+                    2, 2, 2, 2]
+    else:
+        mysample = [sinus2, sinus1+sinus2, sinus1, rand(),
+            sinus1, sinus1, rand(), sinus1]
+    outlet.push_sample(mysample)
+
+    i += 1
+
+
+#%% TEST fft ploting  TODO: (ALEX)
 
 
 
