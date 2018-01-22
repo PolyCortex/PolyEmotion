9c5b071fba5148ca315ede93cc64874b36885caa
Christophe Kedzierski-Tanguay <CKTanguay@gmail.com>
2018-01-22 16:39:32 -0500

    commit de mise a jour

--- MusicPlayerGUI/MusicPlayerGUI/Form1.Designer.cs
+++ MusicPlayerGUI/MusicPlayerGUI/Form1.Designer.cs
@@ -55,10 +55,11 @@
             this.chart1.Location = new System.Drawing.Point(13, 42);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(601, 147);
+            this.chart1.Size = new System.Drawing.Size(601, 222);
             this.chart1.TabIndex = 3;
             this.chart1.Text = "chart1";
             // 
