0c4b0bf6fcd0680a66de5e538514db8411940646
Christophe Kedzierski-Tanguay <CKTanguay@gmail.com>
2018-01-22 17:13:56 -0500

    Chris - Update rapide de l'interface avant la rencontre.

---
+++ MusicPlayerGUI/MusicPlayerGUI/MusicPlayerMainView.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MusicPlayerGUI
+{
+    partial class MusicPlayerGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayerGUI));
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.songPlaylist = new System.Windows.Forms.ListBox();
+            this.btnUpdatePlaylist = new System.Windows.Forms.Button();
+            this.labelPlaylist = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(516, 192);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(13, 42);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(504, 221);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(13, 321);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(504, 260);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            // 
+            // songPlaylist
+            // 
+            this.songPlaylist.FormattingEnabled = true;
+            this.songPlaylist.ItemHeight = 16;
+            this.songPlaylist.Items.AddRange(new object[] {
+            "element 1",
+            "element 2",
+            "element 3",
+            "element 4"});
+            this.songPlaylist.Location = new System.Drawing.Point(516, 353);
+            this.songPlaylist.Name = "songPlaylist";
+            this.songPlaylist.ScrollAlwaysVisible = true;
+            this.songPlaylist.Size = new System.Drawing.Size(153, 180);
+            this.songPlaylist.TabIndex = 5;
+            // 
+            // btnUpdatePlaylist
+            // 
+            this.btnUpdatePlaylist.Location = new System.Drawing.Point(516, 532);
+            this.btnUpdatePlaylist.Name = "btnUpdatePlaylist";
+            this.btnUpdatePlaylist.Size = new System.Drawing.Size(153, 49);
+            this.btnUpdatePlaylist.TabIndex = 6;
+            this.btnUpdatePlaylist.Text = "Update Playlist";
+            this.btnUpdatePlaylist.UseVisualStyleBackColor = true;
+            this.btnUpdatePlaylist.Click += new System.EventHandler(this.btnUpdatePlaylist_Click);
+            // 
+            // labelPlaylist
+            // 
+            this.labelPlaylist.AutoSize = true;
+            this.labelPlaylist.Location = new System.Drawing.Point(559, 321);
+            this.labelPlaylist.Name = "labelPlaylist";
+            this.labelPlaylist.Size = new System.Drawing.Size(52, 17);
+            this.labelPlaylist.TabIndex = 7;
+            this.labelPlaylist.Text = "Playlist";
+            // 
+            // MusicPlayerGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 716);
+            this.Controls.Add(this.labelPlaylist);
+            this.Controls.Add(this.btnUpdatePlaylist);
+            this.Controls.Add(this.songPlaylist);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MusicPlayerGUI";
+            this.Text = "MusicPlayerGUI";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox songPlaylist;
+        private System.Windows.Forms.Button btnUpdatePlaylist;
+        private System.Windows.Forms.Label labelPlaylist;
+    }
+}
+
---
+++ MusicPlayerGUI/MusicPlayerGUI/MusicPlayerMainView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicPlayerGUI
+{
+    public partial class MusicPlayerGUI : Form
+    {
+        public MusicPlayerGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpdatePlaylist_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
